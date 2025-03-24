019e062ecffff1dd1469a6e809a4511a8bbb0ae3
datguyaymen <the.aymen.benzarti@gmail.com>
2025-03-24 13:09:51 +0100

    correcting error in commited code for sign in

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -7,36 +7,49 @@ import { JWT_EXPIRE, JWT_SECRET } from "../config/env.js";
 export const signUp = async (req, res, next) => {
   const session = await mongoose.startSession();
   session.startTransaction();
+
   try {
     const { name, email, password } = req.body;
+
+    // Check if a user already exists
     const existingUser = await User.findOne({ email });
+
     if (existingUser) {
       const error = new Error("User already exists");
       error.statusCode = 409;
       throw error;
     }
+
+    // Hash password
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(password, salt);
-    const newUsers = new User([{ name, email, password: hashedPassword }], {
-      session,
-    });
-    const token = jwt.sign({ id: newUsers[0]._id }, JWT_SECRET, {
+
+    const newUsers = await User.create(
+      [{ name, email, password: hashedPassword }],
+      { session }
+    );
+
+    const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
       expiresIn: JWT_EXPIRE,
-    });
-    session.endSession();
-    res.status(201).json({
-      success: true,
-      message: "User created successfully",
-      data: token,
-      User: newUsers[0],
     });
 
     await session.commitTransaction();
+    session.endSession();
+
+    res.status(201).json({
+      success: true,
+      message: "User created successfully",
+      data: {
+        token,
+        user: newUsers[0],
+      },
+    });
   } catch (error) {
     await session.abortTransaction();
     session.endSession();
     next(error);
   }
 };
+
 export const signIn = async (req, res) => {};
 export const signOut = async (req, res) => {};
